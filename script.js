d1a1a349e7fcc70198a0498a75050706d0b7ae20
Harshithadale <daleharshi6045@gmail.com>
2025-03-12 18:51:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function isSameType(value1, value2) {
-	value1 = isNaN(Number(value1)) ? value1 : Number(value1);
- value2 = isNaN(Number(value2)) ? value2 : Number(value2)
-  if (Number.isNaN(value1) && Number.isNaN(value2)) {
-    return true;
-  } else if (typeof value1 === typeof value2) {
-    return true;
-  } else {
-    return false;
-  }
+    value1 = isNaN(Number(value1)) ? value1 : Number(value1);
+    value2 = isNaN(Number(value2)) ? value2 : Number(value2);
+    if (Number.isNaN(value1) && Number.isNaN(value2)) {
+        return true;
+    } else if (typeof value1 === typeof value2) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 let value1 = prompt("Enter Start of the Range."); 
